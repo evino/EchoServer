79f0adbfebe00f8fa57023d41a3d6697082866db
Evin <evinodisho@gmail.com>
2024-09-10 00:18:23 -0700

    Cleaning up

--- client.c
+++ client.c
@@ -92,45 +92,12 @@ int Client_Connect(int sockfd) {
 
 
 int main() {
-    // Socket used to communicate with server
-    // int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    // GetServerIP();
-
-
     int sockfd = Client_Socket_Init();
     printf("Client socket is %d\n", sockfd);
 
     if (sockfd == -1) {
         printf("sockfd() error: %d\n", errno);
     }
-
-    // // Defining server address
-    // struct sockaddr_in serv_addr;
-    // serv_addr.sin_family = AF_INET;
-    // serv_addr.sin_port = htons(SERVER_PORT);
-    // unsigned int servAddrSize = sizeof(serv_addr);
-
-    // char serverIPAddr[16] = {0};
-
-    // fwrite("Enter server IP: ", sizeof(char), strlen("Enter server IP: "), stdout);
-    // // fread(serverIPAddr, 32, sizeof(char), stdin);
-    // fgets(serverIPAddr, 16, stdin);
-    // serverIPAddr[strcspn(serverIPAddr, "\n")] = '\0';
-
-
-
-    // // Convert IPv4 and IPv6 addresses from text to binary form
-    // if (inet_pton(AF_INET, serverIPAddr, &serv_addr.sin_addr) != 1) {
-    //     printf("inet_pton() returned %d\n", errno);
-    //     return -1;
-    // }
-
-
-    // // Connect to server-fd
-    // if (connect(sockfd, (const struct sockaddr *) &serv_addr, (socklen_t) servAddrSize) != 0) {
-    //     printf("Connection failed with error code %d\n", errno);
-    //     return -1;
-    // }
 
     if (Client_Connect(sockfd) != 0) {
         return -1;
--- server.c
+++ server.c
@@ -14,54 +14,8 @@
 #define BUFFER_SIZE 1024
 
 
-// listener_socket_t *NewSocket(unsigned int port) {
-//     listener_socket_t *sock = malloc(sizeof(listener_socket_t));
-
-//     // Server Address
-//     struct sockaddr_in address;
-
-//     sock->address = &address;
-
-//     // Server-socket creation (listening socket)
-//     sock->fd = socket(AF_INET, SOCK_STREAM, 0);
-//     if (sock->fd < 0) {
-//         // printf("socket() errno is %d\n", errno);
-//         perror("socket() Error");
-//         // return -1;
-//         // exit(-1);
-//         exit(1);
-//         // return;
-//     }
-
-//     // Defining server address
-//     sock->address->sin_family = AF_INET;
-//     sock->address->sin_port = htons(PORT);
-//     sock->address->sin_addr.s_addr = INADDR_ANY;
-//     // unsigned int serverAddrLen = sizeof(address);
-
-//     // Bind socket-fd to socket-address
-//     if (bind(sock->fd, (const struct sockaddr *) &address, sizeof(address)) != 0) {
-//         perror("bind() Error");
-//         // return -1;
-//         exit(1);
-//     }
-
-//     // Put server in listen/passive mode
-//     if (listen(sock->fd, 0) != 0) {
-//         perror("listen() Error");
-//         // printf("listen() errno is %d\n", errno);
-//         // return -1;
-//         exit(1);
-
-//     }
-
-//     return sock;
-// }
-
-
 struct Listener_Socket {
     int fd;
-    // struct sockaddr_in *address;
 };
 
 // GLOBAL Server Address
@@ -75,7 +29,6 @@ int listener_init(listener_socket_t *sock, unsigned int port) {
     sock->fd = socket(AF_INET, SOCK_STREAM, 0);
     printf("Socket made\n");
     if (sock->fd < 0) {
-        // printf("socket() errno is %d\n", errno);
         perror("socket() Error");
         return -1;
     }
@@ -84,7 +37,6 @@ int listener_init(listener_socket_t *sock, unsigned int port) {
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
     address.sin_addr.s_addr = INADDR_ANY;
-    // unsigned int serverAddrLen = sizeof(address);
 
     // Bind socket-fd to socket-address
     if (bind(sock->fd, (const struct sockaddr *) &address, sizeof(address)) != 0) {
@@ -95,7 +47,6 @@ int listener_init(listener_socket_t *sock, unsigned int port) {
     // Put server in listen/passive mode
     if (listen(sock->fd, 0) != 0) {
         perror("listen() Error");
-        // printf("listen() errno is %d\n", errno);
         return -1;
     }
 
@@ -112,7 +63,6 @@ int listener_accept(listener_socket_t *sock) {
 
 int main() {
     listener_socket_t *sock = malloc(sizeof(listener_socket_t));
-    // sock->fd = 0;
 
     if (listener_init(sock, PORT) != 0) {
         return -1;
@@ -125,10 +75,7 @@ int main() {
         printf("connfd: %d\n", connfd);
         char dummy_buf[1] = {0};
         char buffer[BUFFER_SIZE] = {0};
-        // int recv_data = recv(connfd, buffer, BUFFER_SIZE, MSG_PEEK);
         if (recv(connfd, buffer, BUFFER_SIZE, 0) == -1) {
-            // perror("recv() Error");
-            // connfd = accept(serverfd, (struct sockaddr *) &address, (socklen_t *) &serverAddrLen);
             connfd = listener_accept(sock);
             printf("buffer at this point contains: %s\n", buffer);
         }
@@ -143,69 +90,3 @@ int main() {
     }
 
 }
-
-
-
-// int main() {
-//     // Server Address
-//     struct sockaddr_in address;
-
-//     // Server-socket creation (listening socket)
-//     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
-//     if (serverfd < 0) {
-//         printf("socket() errno is %d\n", errno);
-//         return errno;
-//     }
-
-//     printf("serverfd: %d\n", serverfd);
-
-//     // Defining server address
-//     address.sin_family = AF_INET;
-//     address.sin_port = htons(PORT);
-//     address.sin_addr.s_addr = INADDR_ANY;
-//     unsigned int serverAddrLen = sizeof(address);
-
-
-//     // Bind socket-fd to socket-address
-//     if (bind(serverfd, (const struct sockaddr *) &address, sizeof(address)) != 0) {
-//         printf("bind() failed iwth errno %d\n", errno);
-//         perror("bind() Error");
-//         return -1;
-//     }
-
-
-//     // Put server in listen/passive mode
-//     if (listen(serverfd, 0) != 0) {
-//         printf("listen() errno is %d\n", errno);
-//         return errno;
-//     }
-
-
-//     // Gets connection-fd after accepting connection
-//     int connfd = 0;
-//     connfd = accept(serverfd, (struct sockaddr *) &address, (socklen_t *) &serverAddrLen);
-
-//     while (1) {
-//         printf("connfd: %d\n", connfd);
-//         char dummy_buf[1] = {0};
-//         char buffer[BUFFER_SIZE] = {0};
-//         // int recv_data = recv(connfd, buffer, BUFFER_SIZE, MSG_PEEK);
-//         if (recv(connfd, buffer, BUFFER_SIZE, 0) == -1) {
-//             // perror("recv() Error");
-//             connfd = accept(serverfd, (struct sockaddr *) &address, (socklen_t *) &serverAddrLen);
-//             printf("buffer at this point contains: %s\n", buffer);
-//         }
-
-//         if (send(connfd, dummy_buf, 1, 0) == -1) {
-//             fwrite("Connfd closed\n", sizeof(char), strlen("Connfd closed\n"), stdout);
-//             break;
-//         }
-
-
-//         fwrite(buffer, sizeof(char), strlen(buffer), stdout);
-//     }
-
-
-
-//     return 0;
-// }
